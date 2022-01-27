29504a977ebfbf4c11cd6214c6d5158d2dc78e4a
Beksultanova <anjelika.beksultanova.kk@gmail.com>
2022-01-27 13:46:16 +0600

    first commit

---
+++ js/script2.js
@@ -0,0 +1,6 @@
+// 2. Простые сообщения: сохраните сообщение в переменной и выведите это сообщение.Затем замените значение переменной другим сообщением и выведите новое сообщение
+let frstMess = "How are you? <br>";
+document.write(frstMess);
+
+frstMess = "what do you doing? <br>"
+document.write(frstMess)
---
+++ js/script3.js
@@ -0,0 +1,5 @@
+// 3. Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.
+let myName = "Anjelika";
+let myEmailAdd = "anjelika.beksultanova.kk@gmail.com";
+
+document.write(`My name is <strong>${myName}</strong>, and my email address is <strong>${myEmailAdd}</strong><br>`);
